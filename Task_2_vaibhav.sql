87d192ce10dc9a5bc311ae92aadb15ba49917376
VaibhavKoneti <vaibhavkoneti@gmail.com>
2023-02-01 19:14:20 +0100

    Made changes vaibhav

--- Task_2_vaibhav.sql
+++ Task_2_vaibhav.sql
@@ -27,6 +27,39 @@ group by ShipCountry,year)
 group by year,sales_Quaters
 ORDER by year,sales_Quaters;
 
+--Corrected Sales amount Query!!
+/*
+  Revised the corrections and made some changes.
+  country wise sales in each year are taken int high,middle,low sales.
+  Null dates are ignored as we are looking for 2016,2017,2018.
+ */
+with sales_per_year as
+(select o.ShipCountry ,STRFTIME('%Y',o.ShippedDate) as year ,
+round(sum((1 - od.Discount) * od.UnitPrice * od.Quantity)) as Sales_Amount
+from Orders o  
+join "Order Details" od on o.OrderID =od.OrderID
+group by year,o.ShipCountry 
+having year is not null)
+select '(' || group_concat(DISTINCT ShipCountry) || ')' as countries,year,'(' || group_concat(Sales_Amount) || ')' as Sales,
+case 
+	when Sales_Amount < 6500 then "Low sales"
+	when Sales_Amount >6500 and Sales_Amount < 14300 then "Medium Sales" 
+	else "High Sales"
+end as sales_Quaters
+from sales_per_year
+group by year,sales_Quaters;
+
+
+/* 
+ * Null valus has been ignored and the rest year wise results shown below :-
+ */
+
+select STRFTIME('%Y',o.ShippedDate) as year ,
+round(sum((1 - od.Discount) * od.UnitPrice * od.Quantity)) as Sales_Amount
+from Orders o  
+join "Order Details" od on o.OrderID =od.OrderID
+group by year
+having year is not null;
 
 
 
