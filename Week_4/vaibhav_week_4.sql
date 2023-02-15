59046bf4ca4fc1ad7589df4a74a1c3c18691c65d
VaibhavKoneti <vaibhavkoneti@gmail.com>
2023-02-15 11:54:27 +0100

    Changed as per required

--- Week_4/vaibhav_week_4.sql
+++ Week_4/vaibhav_week_4.sql
@@ -24,14 +24,14 @@ join "Order Details" od on o.OrderID =od.OrderID
 join Products p on p.ProductID =od.ProductID 
 join categories c on c.CategoryID  = p.CategoryID 
 group by year,o.ShipCountry )
-select '(' || group_concat(CategoryName) || ')' as Category_List ,'(' || group_concat(Sales_Amount) || ')' as Sales,
+select ShipCountry ,'(' || group_concat(CategoryName) || ')' as Category_List ,'(' || group_concat(Sales_Amount) || ')' as Sales,
 case 
 	when Sales_Amount < 19500 then "Low sales"
 	when Sales_Amount >19500 and Sales_Amount < 42900 then "Medium Sales" 
 	else "High Sales"
 end as sales_Quaters
 from sales_per_year
-group by year,sales_Quaters
+group by ShipCountry 
 having sales_Quaters="Low sales";
 
 
